0c04a4781d0194a170eda24c07a18532e3d26315
Ouissal <OuissalEss@github.com>
2023-08-18 03:31:45 -0400

    update

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ all: $(NAME)
 
 
 $(NAME):
-	mkdir -p /Users/ouissal/Desktop/data/vl_wp
-	mkdir -p /Users/ouissal/Desktop/data/vl_db
+	mkdir -p /home/oessamdi/data/vl_wp
+	mkdir -p /home/oessamdi/data/vl_db
 	docker-compose -f ./srcs/docker-compose.yml up --build
 
 down:
@@ -20,9 +20,9 @@ prune:
 		docker-compose -f ./srcs/docker-compose.yml down --rmi all --volumes
 
 clean:
-	rm -rf /Users/ouissal/Desktop/data/vl_wp
-	rm -rf /Users/ouissal/Desktop/data/vl_db
-	rm -rf /Users/ouissal/Desktop/data
+	rm -rf /home/oessamdi/data/vl_wp
+	rm -rf /home/oessamdi/data/vl_db
+	rm -rf /home/oessamdi/data
 
 fclean: prune clean
 
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,11 +2,11 @@
 
 service mysql start
 
-echo "CREATE DATABASE $MYSQL_DATABASE;" >> /tmp/init.sql
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /tmp/init.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';" >> /tmp/init.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';" >> /tmp/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmp/init.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
 
 mysql < /tmp/init.sql
